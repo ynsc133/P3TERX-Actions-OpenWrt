ff245f87a4fc3a7a9dbf9ac093a6014f16b6a7df
ynsc133 <ynsc133@users.noreply.github.com>
2021-08-06 17:46:24 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -16,3 +16,11 @@
 # Add a feed source
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+
+cd ./lede/lean
+git clone https://github.com/esirplayground/luci-app-poweroff.git
+git clone https://github.com/sirpdboy/luci-app-autopoweroff.git
+git clone https://github.com/AlexZhuo/luci-app-bandwidthd.git
+git clone https://github.com/tty228/luci-app-serverchan.git
+git clone https://github.com/small-5/luci-app-adblock-plus.git
+git clone https://github.com/rufengsuixing/luci-app-adguardhome.git
