1aeebef42b4dff0f8e205412af134e16febf70c6
ynsc133 <ynsc133@users.noreply.github.com>
2021-08-07 13:52:05 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -9,6 +9,13 @@
 # File name: diy-part2.sh
 # Description: OpenWrt DIY script part 2 (After Update feeds)
 #
-
+mv ./openwrt/feeds/luci/collections/luci/Makefile ./openwrt/feeds/luci/collections/luci/Makefile.bak
+cp ./Makefile ./openwrt/feeds/luci/collections/luci
+mv ./openwrt/package/base-files/files/etc/shadow ./openwrt/package/base-files/files/etc/shadow.bk
+cp ./shadow ./openwrt/package/base-files/files/etc
+mv ./openwrt/package/feeds/luci/luci-base/root/www/index.html ./openwrt/package/feeds/luci/luci-base/root/www/index.html.bk
+cp ./index.html ./openwrt/package/feeds/luci/luci-base/root/www
+mv ./openwrt/package/base-files/files/bin/config_generate ./openwrt/package/base-files/files/bin/config_generate.bk
+cp ./config_generate ./openwrt/package/base-files/files/bin
 # Modify default IP
-#sed -i 's/192.168.1.1/192.168.1.10/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.1.10/g' package/base-files/files/bin/config_generate
