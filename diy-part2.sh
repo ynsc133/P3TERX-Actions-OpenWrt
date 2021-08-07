5a019abd05264b43df57288f7c45ac007bcec58f
ynsc133 <ynsc133@users.noreply.github.com>
2021-08-07 13:59:58 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -9,13 +9,15 @@
 # File name: diy-part2.sh
 # Description: OpenWrt DIY script part 2 (After Update feeds)
 #
-mv ./openwrt/feeds/luci/collections/luci/Makefile ./openwrt/feeds/luci/collections/luci/Makefile.bak
-cp ./Makefile ./openwrt/feeds/luci/collections/luci
-mv ./openwrt/package/base-files/files/etc/shadow ./openwrt/package/base-files/files/etc/shadow.bk
-cp ./shadow ./openwrt/package/base-files/files/etc
-mv ./openwrt/package/feeds/luci/luci-base/root/www/index.html ./openwrt/package/feeds/luci/luci-base/root/www/index.html.bk
-cp ./index.html ./openwrt/package/feeds/luci/luci-base/root/www
-mv ./openwrt/package/base-files/files/bin/config_generate ./openwrt/package/base-files/files/bin/config_generate.bk
-cp ./config_generate ./openwrt/package/base-files/files/bin
+mv ./feeds/luci/collections/luci/Makefile ./feeds/luci/collections/luci/Makefile.bak
+cp ./Makefile ./feeds/luci/collections/luci
+mv ./package/base-files/files/etc/shadow ./package/base-files/files/etc/shadow.bk
+cp ./shadow ./package/base-files/files/etc
+mv ./package/feeds/luci/luci-base/root/www/index.html ./package/feeds/luci/luci-base/root/www/index.html.bk
+cp ./index.html ./package/feeds/luci/luci-base/root/www
+mv ./package/base-files/files/bin/config_generate ./package/base-files/files/bin/config_generate.bk
+cp ./config_generate ./package/base-files/files/bin
+mv ./package/kernel/mac80211/files/lib/wifi/mac80211.sh ./package/kernel/mac80211/files/lib/wifi/mac80211.sh.bk
+cp ./mac80211.sh ./package/kernel/mac80211/files/lib/wifi
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.1.10/g' package/base-files/files/bin/config_generate
